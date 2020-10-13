c9a603583c55fe5a5b682f848efd499aed58ec3e
skarmen <karmennsalim@gmail.com>
2020-10-13 15:21:24 -0400

    Updates

---
+++ precache-manifest.1b03a4217f4ab85b60019a46bfa706a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "169c58a8743e9e8b1bd03ab000d9c01d",
+    "url": "/online-store/index.html"
+  },
+  {
+    "revision": "5e87c0f2c9e8051e439d",
+    "url": "/online-store/static/css/main.121ac642.chunk.css"
+  },
+  {
+    "revision": "d6506c0bc95abeefe057",
+    "url": "/online-store/static/js/2.fbe5559d.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/online-store/static/js/2.fbe5559d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e87c0f2c9e8051e439d",
+    "url": "/online-store/static/js/main.9995ee7d.chunk.js"
+  },
+  {
+    "revision": "a11e96ba6ee438d78cdb",
+    "url": "/online-store/static/js/runtime-main.e28376a6.js"
+  },
+  {
+    "revision": "bb77082c132b8c11a660c3733b4d48e3",
+    "url": "/online-store/static/media/banner-2.bb77082c.jpg"
+  }
+]);
